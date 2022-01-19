a5ba97c338c5bed7eab85c84bccac7c73dc61139
Bernardo <bernardo.cabral@outlook.com>
2022-01-19 12:23:40 -0100

    ..

--- src/model/routes/fileRoutes/styleRouter.js
+++ src/model/routes/fileRoutes/styleRouter.js
@@ -1,5 +1,23 @@
 const srcLocation = require("../../../srcLocation");
+const path = require("path");
 const express = require("express");
 const router = express.Router();
+
+
+router.get("/style.css",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/CSS/style.css"));
+})
+
+router.get("/login.css",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/CSS/login.css"));
+})
+
+router.get("/gerirRecursos.css",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/CSS/gerirRecursos.css"));
+})
+
+router.get("/ofertas.css",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/CSS/ofertas.css"));
+})
 
 module.exports = router;
--- src/model/routes/mainRouter.js
+++ src/model/routes/mainRouter.js
@@ -1,4 +1,5 @@
 const srcLocation = require("../../srcLocation");
+const path = require("path");
 const express = require("express");
 const router = express.Router();
 
@@ -9,9 +10,53 @@ router.use("/api",apiRouter);
 const fileRouter = require("./fileRouter");
 router.use("/files",fileRouter);
 
+
+//todos
 router.get("/",(req,res)=>{
-    res.send("root page"); 
-    console.log()
+    res.sendFile(path.join(srcLocation,"/view/HTML/home.html"));
 })
 
+router.get("/login",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/HTML/login.html"));
+})
+
+router.get("/perfil",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/HTML/perfil.html"));
+})
+
+
+//Alunos
+router.get("/ofertas",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/HTML/ofertas.html"));
+})
+
+
+//empresa
+router.get("/registo",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/HTML/registoEmpresa.html"));
+})
+
+router.get("/minha-area",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/HTML/minhaArea.html"));
+})
+
+router.get("/minha-area/gerir-anuncio",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/HTML/gerirAnuncio.html"));
+})
+
+router.get("/minha-area/criar-anuncio",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/HTML/criarAnuncio.html"));
+})
+
+
+//ADMIN e DIRETOR
+router.get("/gerir-recursos",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/HTML/gerir-recursos.html"));
+})
+
+
+//ADMIN
+router.get("/criar-recursos",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/HTML/gestao.html"));
+})
 module.exports = router;
