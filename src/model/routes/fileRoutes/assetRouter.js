48a45de276275c8b2c91cd06d6bcacf43036b5b1
diogobranco38 <diogobranco38@gmail.com>
2022-01-19 16:23:24 -0100

    .

--- src/model/routes/fileRoutes/assetRouter.js
+++ src/model/routes/fileRoutes/assetRouter.js
@@ -1,5 +1,34 @@
 const srcLocation = require("../../../srcLocation");
+const path = require("path");
 const express = require("express");
 const router = express.Router();
+
+router.get("/logoEnta.png",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/logoEnta.png"));
+})
+
+router.get("/pessoa.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/pessoa.svg"));
+})
+
+router.get("/profile.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/profile.svg"));
+})
+
+router.get("/criarAnuncio.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/criarAnuncio.svg"));
+})
+
+router.get("/criarEmpre.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/criarEmpre.svg"));
+})
+
+router.get("/dados.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/dados.svg"));
+})
+
+router.get("/profile.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/profile.svg"));
+})
 
 module.exports = router;
