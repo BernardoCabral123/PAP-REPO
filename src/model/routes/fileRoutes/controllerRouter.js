5b1e862bca8eca96adef6375ee05d0c32749b8a0
diogobranco38 <diogobranco38@gmail.com>
2022-01-25 10:10:53 -0100

    .

--- src/model/routes/fileRoutes/controllerRouter.js
+++ src/model/routes/fileRoutes/controllerRouter.js
@@ -8,5 +8,9 @@ router.get("/navFoot.js",(req,res)=>{
     res.sendFile(path.join(srcLocation,"/controller/navFoot.js"));
 })
 
+router.get("/script.js",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/controller/script.js"));
+})
+
 
 module.exports = router;
