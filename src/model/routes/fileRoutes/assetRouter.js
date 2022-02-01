b46f3cd542ba595a0a18622f1a7a0eef6ea21e87
diogobranco38 <diogobranco38@gmail.com>
2022-02-01 18:43:19 -0100

    .

--- src/model/routes/fileRoutes/assetRouter.js
+++ src/model/routes/fileRoutes/assetRouter.js
@@ -31,6 +31,10 @@ router.get("/criarEmpre.svg",(req,res)=>{
     res.sendFile(path.join(srcLocation,"/view/Assets/criarEmpre.svg"));
 })
 
+router.get("/logoPlataforma.png",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/logoPlataforma.png"));
+})
+
 router.get("/criarAluno.svg",(req,res)=>{
     res.sendFile(path.join(srcLocation,"/view/Assets/criarAluno.svg"));
 })
