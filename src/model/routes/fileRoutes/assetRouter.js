61cd41fe21599cc8e64001b643144eb6a0791b53
diogobranco38 <diogobranco38@gmail.com>
2022-02-24 16:53:17 -0100

    .

--- src/model/routes/fileRoutes/assetRouter.js
+++ src/model/routes/fileRoutes/assetRouter.js
@@ -3,105 +3,14 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 
-
-router.get("/logoEnta.png",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/logoEnta.png"));
-})
-
-router.get("/pessoa.svg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/pessoa.svg"));
-})
-
-router.get("/diretorTurma.svg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/diretorTurma.svg"));
-})
-
-router.get("/edificio.png",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/edificio.png"));
-})
-
-router.get("/profile.svg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/profile.svg"));
-})
-
-router.get("/criarAnuncio.svg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/criarAnuncio.svg"));
-})
-
-router.get("/criarEmpre.svg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/criarEmpre.svg"));
-})
-
-router.get("/logoPlataforma.png",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/logoPlataforma.png"));
-})
-
-router.get("/logoPlataformaPrincipal.png",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/logoPlataformaPrincipal.png"));
-})
-
-router.get("/criarAluno.svg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/criarAluno.svg"));
-})
-
-router.get("/dados.png",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/dados.png"));
-})
-
-router.get("/MUSAMI.jpg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/MUSAMI.jpg"));
-})
-
-router.get("/profile.svg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/profile.svg"));
-})
-
-router.get("/lab.jpg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/lab.jpg"));
-})
-
-router.get("/reuniao.jpg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/reuniao.jpg"));
-})
-
-router.get("/servidores.jpg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/servidores.jpg"));
-})
-
-router.get("/principal.png",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/principal.png"));
-})
+//home
 
 router.get("/work.svg",(req,res)=>{
     res.sendFile(path.join(srcLocation,"/view/Assets/work.svg"));
 })
 
-router.get("/Estagio.svg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/Estagio.svg"));
-})
-
-router.get("/principal.jpg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/principal.jpg"));
-})
-
-router.get("/curso.svg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/curso.svg"));
-})
-
-router.get("/turma.svg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/turma.svg"));
-})
-
-router.get("/CriarConta.png",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/CriarConta.png"));
-})
-
 router.get("/procurar.png",(req,res)=>{
     res.sendFile(path.join(srcLocation,"/view/Assets/procurar.png"));
-})
-
-router.get("/Empresa.png",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/Empresa.png"));
 })
 
 router.get("/numero-1.png",(req,res)=>{
@@ -128,12 +37,81 @@ router.get("/numero-3verde.png",(req,res)=>{
     res.sendFile(path.join(srcLocation,"/view/Assets/numero-3verde.png"));
 })
 
-router.get("/duvidas.svg",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/duvidas.svg"));
+router.get("/userLogin.png",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/userLogin.png"));
 })
 
-router.get("/parceria.png",(req,res)=>{
-    res.sendFile(path.join(srcLocation,"/view/Assets/parceria.png"));
+router.get("/candidate.png",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/candidate.png"));
+})
+
+router.get("/check.png",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/check.png"));
+})
+
+router.get("/Estagio.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/Estagio.svg"));
+})
+
+router.get("/CriarConta.png",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/CriarConta.png"));
+})
+
+//logos
+
+router.get("/logoEnta.png",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/logoEnta.png"));
+})
+
+router.get("/logoPlataforma.png",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/logoPlataforma.png"));
+})
+
+router.get("/logoPlataformaPrincipal.png",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/logoPlataformaPrincipal.png"));
+})
+
+router.get("/MUSAMI.jpg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/MUSAMI.jpg"));
+})
+
+//Criar Conta empresa
+
+router.get("/criarEmpre.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/criarEmpre.svg"));
+})
+
+//criar Anuncio
+router.get("/criarAnuncio.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/criarAnuncio.svg"));
+})
+
+//profile
+router.get("/profile.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/profile.svg"));
+})
+
+//dashboard
+
+router.get("/diretorTurma.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/diretorTurma.svg"));
+})
+
+router.get("/criarAluno.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/criarAluno.svg"));
+})
+
+router.get("/curso.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/curso.svg"));
+})
+
+router.get("/turma.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/turma.svg"));
+})
+
+//duvidas
+router.get("/duvidas.svg",(req,res)=>{
+    res.sendFile(path.join(srcLocation,"/view/Assets/duvidas.svg"));
 })
 
 module.exports = router;
